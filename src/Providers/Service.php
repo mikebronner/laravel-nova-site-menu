9b24e29506ce1628160d654d5ef5450633340332
Mike Bronner <mike@genealabs.com>
2021-04-25 14:57:21 -0700

    Update Service.php

--- src/Providers/Service.php
+++ src/Providers/Service.php
@@ -21,7 +21,7 @@ class Service extends ServiceProvider
         Blade::component('menu-item', MenuItem::class);
         $this->loadViewsFrom(__DIR__ . '/../../resources/views', 'laravel-nova-site-menu');
 
-        if (! MenuModel::ignoreMigrations()) {
+        if (MenuModel::runsMigrations()) {
             $this->loadMigrationsFrom(__DIR__ . '/../../database/migrations');
         }
 
